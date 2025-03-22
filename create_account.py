3ee98f6f0db76bdd7501e8831cfac3020f157c16
Aman Rai <149163942+Amanrai503@users.noreply.github.com>
2025-03-22 19:36:28 +0530

    bug fix

--- create_account.py
+++ create_account.py
@@ -1,12 +1,11 @@
 import sys
-import mysql.connector as sqlt
+from cryptography.fernet import Fernet
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QIcon
 from inset_user import insert_user_info
 from login_logic import get_key, verify
 import resource_1
-from main_window import MainWindow
 
 
 
@@ -31,13 +30,14 @@ class CreateWindow(QMainWindow):
             self.key_va = self.key
             self.qr_label.setPixmap(qr)
             self.qr_label.setScaledContents(True)
+            self.enc_key = Fernet.generate_key()
 
     def verify_and_save(self):
         totp = self.totp.text()
         if verify(self.key, totp):
             print("Verification successful, proceeding to insert user...")
             try:
-                insert_user_info(self.name_val, self.email_val, self.password_val, self.key_va)
+                insert_user_info(self.name_val, self.email_val, self.password_val, self.key_va, self.enc_key)
                 print("Insert function completed")
             except Exception as e:
                 print(f"Error during insert: {e}")
